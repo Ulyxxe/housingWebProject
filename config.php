342af8bc263ee473a51713c159e82e10abc4c4cf
Ulyxxe <116708941+Ulyxxe@users.noreply.github.com>
2025-04-11 16:00:30 +0200

    💚 fix env

--- config.php
+++ config.php
@@ -1,11 +1,10 @@
 <?php
 // config.php
 // Load environment variables if you are using a .env file with phpdotenv, or simply define them here.
-$host = 'localhost';
-$dbname = 'your_database';
-$username = 'your_username';
-$password = 'your_password';
-
+$host   = getenv('MYSQL_HOST');
+$dbname = getenv('MYSQL_DATABASE');
+$username = getenv('MYSQL_USER');
+$password = getenv('MYSQL_PASSWORD');
 try {
     $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
     $pdo = new PDO($dsn, $username, $password);
